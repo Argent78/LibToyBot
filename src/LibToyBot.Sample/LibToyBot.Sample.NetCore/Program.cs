7c7331230837eb8cf6bd91b11d69949696506315
Aaron <argent14@bigpond.com>
2020-01-01 09:53:30 +0800

    Added project documentation to README
     * Test coverage
    
     * Azure pipelines build and branching strategy
    
     * .NET Core Sample project
    
     * Test case for lower case input commands
    
     * nuget config and how to install library

---
+++ src/LibToyBot.Sample/LibToyBot.Sample.NetCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using LibToyBot.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibToyBot.Sample.NetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PlaceAndMoveRobot();
+        }
+
+        public static void PlaceAndMoveRobot()
+        {
+            var robot = GetRobot();
+            robot.Action("PLACE 1,2,EAST");
+            robot.Action("MOVE"); // 2,2,EAST
+            robot.Action("LEFT"); // 2,2,NORTH
+            robot.Action("MOVE");
+            robot.Action("MOVE"); // 2,4,NORTH
+            robot.Action("REPORT"); // Print position & orientation
+        }
+
+        private static Robot GetRobot()
+        {
+            return new ServiceCollection()
+                .AddRobot()
+                .BuildServiceProvider()
+                .GetService<Robot>();
+        }
+    }
+}
